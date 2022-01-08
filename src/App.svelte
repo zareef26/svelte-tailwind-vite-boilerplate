4a0158e2fc5efa99cfa41874e40f7d9c5041d95a
Adnan Zareef <adnanzareef@outlook.com>
2022-01-08 17:17:19 +0600

    boilerplate creation

---
+++ src/App.svelte
@@ -0,0 +1,7 @@
+<script>
+	export let hey
+</script>
+
+<main>
+	<h1 class="text-6xl bg-sky-500 p-5 flex justify-center items-center">{hey}</h1>
+</main>
---
+++ src/main.js
@@ -0,0 +1,11 @@
+import App from './App.svelte'
+import './index.css'
+
+const app = new App({
+  target: document.getElementById('app'),
+  props: {
+    hey: "hey"
+  }
+})
+
+export default app
